a6c132845fd6b9b1820dc6d96521c122e9f3a9cd
mutasem <motasemimad10@gmail.com>
2021-03-22 14:40:20 +0200

    add migration create-shifts-table

---
+++ database/migrations/2021_03_22_123017_create-shifts-table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShiftsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('shifts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->time('start_at')->nullable();
+            $table->time('end_at')->nullable();
+            $table->boolean('saturday')->default(1);
+            $table->boolean('sunday')->default(1);
+            $table->boolean('mondayday')->default(1);
+            $table->boolean('tuesday')->default(1);
+            $table->boolean('wednesday')->default(1);
+            $table->boolean('thursday')->default(1);
+            $table->boolean('friday')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
